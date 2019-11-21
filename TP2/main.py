ef2b79addbf5b464632cae2308f97332c7c67373
Ragib <ahashanragib@gmail.com>
2019-11-21 18:07:25 -0500

    Reading file works

--- TP2/main.py
+++ TP2/main.py
@@ -4,26 +4,34 @@ from FiniteStateMachine import Automate
 from OrderManager import OrderManager
 
 
-
+# WORKS
 def read_file(file_name = 'inventaire.txt'):
 	items_list = []
-
 	try:
 		with open(file_name) as csv_file:
 			csv_reader = csv.reader(csv_file, delimiter=' ')
 			for line in csv_reader:
 				print(len(line))
-                #name = line[0]
+				name = str(line[0])
+				id_code = str(line[1])
+				type_object = str(line[2])
+				item = Object(name, id_code, type_object)
+				items_list.append(item)
+                
 		print("Le ficher est lu!")
 	except Exception as e:
 		print("Erreur de lecture du fichier! Le fichier '" + file_name + "' ne peut pas être lu.")
 
+	return items_list
 
 
 
 
 def main():
-    read_file()
+	print("Main")
+	items_store = read_file()
+
+	
 
 
 
